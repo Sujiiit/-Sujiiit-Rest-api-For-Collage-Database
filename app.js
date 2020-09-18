d1e586697d429da019d805a01e9108143f2fabed
sujiiit <sujiiitmishra@gmail.com>
2020-09-19 04:31:36 +0530

    'process.env.portsetv2.0'

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ let express,app,mongoose;
    let StarterFile = require('./Starter');
    
    // Listning on port 5000
-   app.listen(5000, ( ()  => { console.log('Server started on port 5000.');  }));
+   app.listen(process.env.PORT, ( ()  => { console.log('Server started on port 5000.');  }));
    
  
 
