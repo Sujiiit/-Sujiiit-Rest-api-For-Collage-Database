b5d105ee11453838856b7b9593d5dc5235b0df8e
sujiiit <sujiiitmishra@gmail.com>
2020-09-19 04:22:15 +0530

    'Rest-apiv1.0'

---
+++ Starter.js
@@ -0,0 +1,17 @@
+let list = [ { ROLL_NO: '', NAME: '', CLASS: '',SECTION: '',DOB: '',Admission_No: '',EMAIL: '',PASSWORD: '',first: '',second: '',third: '',forth: '',fifth: '',sixth: ''},
+{ ROLL_NO: '', NAME: '', CLASS: '',SECTION: '',DOB: '',Admission_No: '',EMAIL: '',PASSWORD: '',first: '',second: '',third: '',forth: '',fifth: '',sixth: ''},
+{ ROLL_NO: '', NAME: '', CLASS: '',SECTION: '',DOB: '',Admission_No: '',EMAIL: '',PASSWORD: '',first: '',second: '',third: '',forth: '',fifth: '',sixth: ''},
+{ ROLL_NO: '', NAME: '', CLASS: '',SECTION: '',DOB: '',Admission_No: '',EMAIL: '',PASSWORD: '',first: '',second: '',third: '',forth: '',fifth: '',sixth: ''},
+{ ROLL_NO: '', NAME: '', CLASS: '',SECTION: '',DOB: '',Admission_No: '',EMAIL: '',PASSWORD: '',first: '',second: '',third: '',forth: '',fifth: '',sixth: ''},
+{ ROLL_NO: '', NAME: '', CLASS: '',SECTION: '',DOB: '',Admission_No: '',EMAIL: '',PASSWORD: '',first: '',second: '',third: '',forth: '',fifth: '',sixth: ''},
+{ ROLL_NO: '', NAME: '', CLASS: '',SECTION: '',DOB: '',Admission_No: '',EMAIL: '',PASSWORD: '',first: '',second: '',third: '',forth: '',fifth: '',sixth: ''},
+{ ROLL_NO: '', NAME: '', CLASS: '',SECTION: '',DOB: '',Admission_No: '',EMAIL: '',PASSWORD: '',first: '',second: '',third: '',forth: '',fifth: '',sixth: ''},
+{ ROLL_NO: '', NAME: '', CLASS: '',SECTION: '',DOB: '',Admission_No: '',EMAIL: '',PASSWORD: '',first: '',second: '',third: '',forth: '',fifth: '',sixth: ''},
+{ ROLL_NO: '', NAME: '', CLASS: '',SECTION: '',DOB: '',Admission_No: '',EMAIL: '',PASSWORD: '',first: '',second: '',third: '',forth: '',fifth: '',sixth: ''} ];
+
+ let Source = [{titlename: 'ROLL_NO'},{titlename: 'NAME'},{titlename: 'CLASS'},{titlename: 'SECTION'},
+{titlename:'DOB'},{titlename:'Admission_No'},{titlename:'EMAIL'},{titlename:'PASSWORD'},{titlename: ''}
+,{titlename: ''},{titlename: ''},{titlename: ''},{titlename: ''},{titlename: ''}];
+
+
+module.exports = { list: list,Source: Source };
--- app.js
+++ app.js
@@ -1,157 +1,111 @@
-let express,app,bodyparser,mongoose,_,globalid;
+let express,app,mongoose;
 
-  express = require('express');
-  bodyparser = require('body-parser');
-  mongoose = require('mongoose');
-  _ = require('lodash');
-
-  app = express();
-  app.set("view engine", "ejs"); //to use the ejs 
-  app.use(bodyparser.urlencoded({extended: true}));
-  app.use(express.static('Public'));
-
-   // Image uploading tools
-
-   let fs = require('fs'); 
-   let path = require('path'); 
-   require('dotenv/config'); 
-   let multer = require('multer'); 
-
-    
-    // Data for the post Route
-
-  let storage = multer.diskStorage({ 
-      destination: (req, file, cb) => { 
-      cb(null, 'uploads') 
-     }, 
-      filename: (req, file, cb) => { 
-      cb(null, file.fieldname + '-' + Date.now()) 
-     } 
-  }); 
-
-  let upload = multer({ storage: storage }); 
-
-
-      // Creating  Database Todolist
-    mongoose.connect('mongodb://localhost:27017/Blog',{ useNewUrlParser: true ,useUnifiedTopology: true }); 
-
-
-     // Creating collection ( post )
-    const postschema = {  title: String , content: String , key: Number , img: {data: Buffer,  contentType: String }  };
-    const Post = mongoose.model('Post',postschema);
-
+   express = require('express');
+   mongoose = require('mongoose');
+   app = express();
+   app.use(express.urlencoded({ extended: true }));
+   app.use(express.json());
+   let StarterFile = require('./Starter');
+   
+   // Listning on port 5000
+   app.listen(5000, ( ()  => { console.log('Server started on port 5000.');  }));
+   
  
 
-     let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-     let today  = new Date(),  date = today.toLocaleDateString("en-US", options);
-
-
-     const Listschema = { name: String , Items: [postschema] }
-     const List = mongoose.model('List',Listschema);
-  
-
+   // Creating  Database SchoolDatabase 
+   //   We are not using any kind of encryption
+    mongoose.connect('mongodb+srv://sujiiit:FAaE434gYofRwvfN@cluster0.rrkr2.mongodb.net/SchoolDatabase',{ useNewUrlParser: true ,useUnifiedTopology: true });
     
-       // Home Get Routes
-     app.get("/",  ( (req,res) => { 
 
-        Post.find({},function(err,foundItems)
-         {
-           if(!err)
-            {  
-             
-             (foundItems.length > 0) ? globalid = Number( foundItems[foundItems.length-1].key ) + 1  :  globalid = 1;
-            	function compare(a,b) 
-            	  {
-            	  	  if (a.key > b.key) return -1;
-                      if (b.key > a.key) return 1;
-                      return 0;
-                     // return a.key - b.key;
-                  }  
-                              
-                 res.render('Main',{Posts: (foundItems.sort(compare)), inside: false}); 
-             
-            } 
-         });
-  	     
-  	 }));
+     let dataSchema = ({
+     ROLL_NO: String, NAME: String, CLASS: String,
+     SECTION: String, DOB: String, Admission_No: String,
+     EMAIL: String, PASSWORD: String, first: '',
+     second: '',third: '',forth: '',fifth: '',sixth: ''
+     }); 
+     let Students = mongoose.model('Students',dataSchema); 
 
-
-
-  app.get('/about',   ( (req,res) =>  { res.render('Aboutus',{Title: 'Aboutus page',paragraph: lorem})  }))
-  app.get('/contact', ( (req,res) =>  { res.render('contact',{Title: 'Contact page',paragraph: lorem})  }))
-  app.get('/compose' ,( (req,res) => { res.render('compose',{Title: 'Compose page'})  }))
+  
+          
+//   Home Get Routes
+   let list = StarterFile.list;
 
      
+     app.get("/Home", function(req, res) {
+        Students.find({},function(error,response){
+        if(!error)
+            {
+               if(!response.length){
+               //  if the database is empty then we create some  empt fields
+                Students.insertMany([...list],function(err){
+                  (!err) && console.log('data entered Successfully.');
+                  });             
+                 
+                    fetchtitles().then((title) => {
+                    res.send([title,list])   
+                    }).catch((message) => {
+                    })
+                         
+               }
+               else{
+                  // Data already exist so we sending them.
+                  fetchtitles().then((title) => {                
+                     res.send([title,response])                
+                     }).catch((title) => {   
+                     })
+               }
+            } 
+            else{
+               console.log(error);
+            }
+        }).limit(10);
+        
+     });
+
    
 
-  app.get('/:parameter', ( (req,res) => {
-
-        let finditem = _.lowerCase(req.params.parameter);
-
-  	    Post.find({},function(err,foundItems) {
-           if(!err) {
-             
-             let foun = foundItems.filter(function(item){ return _.lowerCase(item.title) === finditem})
-             if(foun.length > 0)
-               {
-                  res.render('Main',{Posts: foun, inside: true})
-               }
-                else
-               {
-                  // res.render('post',{Title: "OOP's we can't find page",Content: ''})
-               }
-           } 
-        });
-
-
-    }))
+     app.post("/Home", function(req, res) {
+        let alldata = req.body.alldata,i=0,len = alldata.length,temp=0;
+        console.log(alldata);
+       while(i<len){              
+          if( (i+1) < len ){
+            if(alldata[i].id !== alldata[i+1].id || alldata[i].key !== alldata[i+1].key){
+                savedata(alldata[i].id,alldata[i].key,alldata[i].val);
+             } 
+            
+           }
+           else {
+               savedata(alldata[i].id,alldata[i].key,alldata[i].val);
+          }      
+           i++;
+          }     
+        
+      });
 
 
 
-  app.post('/search', ( (req,res) => { 
+  
+   let TitleSchema =  ({  titlename: String  });
+   let Title = mongoose.model('Title',TitleSchema);
+   const Source =  StarterFile.Source;
+   
+ 
+app.post('/title',function(req,res){
       
+   let alldata = req.body.alldata,i=0,len = alldata.length,temp=0;
+     while(i<len){              
+         if( (i+1) < len ){
 
-        let finditem = _.lowerCase(req.body.title);
+           if(alldata[i].ID !== alldata[i+1].ID){
+           savetitle(alldata[i].ID,alldata[i].V);
+          }    
+        }
+         else {
+               savetitle(alldata[i].ID,alldata[i].V);
+             }      
+        i++;
+      }     
 
-  	     Post.find({},function(err,foundItems) {
-           if(!err) {
-             
-             let foun = foundItems.filter(function(item){ return _.lowerCase(item.title) === finditem})
-             if(foun.length > 0)
-               {
-                  res.render('Main',{Posts: foun, inside: true})
-               }
-                else
-               {
-                  // res.render('post',{Title: "OOP's we can't find page",Content: ''})
-               }
-           } 
-        });
-      
-
-  }))
-
-
-
-
-
-
- app.post('/', upload.single('image'),  (req,res) => { 
-
- 	             
- 	             let nepost = new Post ({
-
- 		            title:  req.body.posttitle,
- 		            content:  req.body.postcontent,
- 		            key:  globalid,
-                img: { 
-                       data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)), 
-                       contentType: 'image/png'
-                     }	
- 	              });
-                  
-                  nepost.save();  
-                  res.redirect('/')
 });
 
 
@@ -160,8 +114,156 @@ let express,app,bodyparser,mongoose,_,globalid;
 
 
 
+function savedata(ID,fieldname,value=''){
+  
+   if(ID.length !== 0){
+   
+   Students.findOne({_id: ID},function(err,response){
+      if(err) {
+          console.log(err);
+      }
+      else  {      
+       Students.updateOne({'_id': ID},{[fieldname]: value}, function(error) {
+              if(error) {  console.log(error) } else { console.log('updated successfully.')}
+           });
+          }    
+
+   });
+ } // Id length
+}
 
 
- app.listen(3000, ( ()  => { console.log('Server started on port 3000.');  }));
 
-	
+
+ function fetchtitles(){
+   var Titls=[];
+    return new Promise((resolve,reject) => {
+        Title.find({}, function(err,response){    
+        if(!err) {
+          if(!response.length){
+            //   Inserting some normal titles
+            Title.insertMany([...Source],function(err){
+            (!err) && console.log('Some basic titles saved.');
+         });            
+               resolve(Source);
+        }    //  response length
+      else{
+          let oue = Object.entries(response);
+          Titls = oue.map((element,index) => {
+          Titls.push(element); 
+          if(index == oue.length-1) { 
+                 resolve(Titls);
+               
+             }    
+      });
+   }   // Else scope
+   
+   }  // Error barcket
+})
+      
+});
+}
+
+
+function savetitle(T,V)  {
+   if(T !== '') {
+   Title.findOne({_id: T},function(err,response){
+      if(!err) {
+           Title.updateOne({'_id': T},{titlename: V}, function(error) {
+              if(error) {  console.log(error) } else { console.log('updated successfully.')}
+           });
+        }  
+   })
+  }
+}
+
+
+app.post('/addpage',(req,res) => {
+   Students.insertMany([...list],function(err){
+      (!err) && console.log('data entered Successfully.');
+      }); 
+      fetchtitles().then((title) => {
+         res.send([title,list])
+         }).catch((message) => {
+         })
+})
+
+
+let page = 0;
+app.get('/nextpage',(req,res) => {
+ page++;
+    Students.find({},function(error,response){
+      if(!error)
+          {
+             if(!response.length){
+             //  if we found nothing then
+                res.send("Page does'nt exist");
+                page--;
+               }
+               else{
+                  res.send(response);
+               }
+            }
+
+            else{
+               console.log(error)
+            }
+      }).skip(page*10).limit(10);
+});
+
+
+
+app.get('/prevpage',(req,res) => {
+   page--;
+   Students.find({},function(error,response){
+     if(!error)
+         {
+            if(!response.length){
+            //  if we found nothing then
+            
+               res.send("Page does'nt exist");
+
+              }
+              else{
+                res.send(response);
+              }
+           }
+     }).skip(page*10).limit(10);
+});
+
+
+app.get('/search',(req,res) => {
+
+    let tofind = req.query.key,found = false; 
+         
+       
+            fetchtitles().then((title) => {
+               title.map((item) => { (!found) && findby(item[1].titlename,tofind)  });
+               });  
+      
+
+       setTimeout(() => {(!found) &&  res.send('nothing found')},500);
+     
+
+  function findby(item,tofind){
+   Students.find({[item]: tofind},function(error,response){
+     
+      if(!error)
+          {
+             if(!response.length){
+             //  if we found nothing then...
+               //  res.send("Page does'nt exist");
+               }
+               else{
+                  
+                  res.send(response); 
+                  found = true; 
+               }
+            }
+
+            else{
+               console.log(error);
+            }
+      })
+    }
+});
